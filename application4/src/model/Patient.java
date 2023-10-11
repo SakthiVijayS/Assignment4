f1f5e88b75e60ca64a02ccbdf96a2f539e9d6f91
SakthiVijayS <124193377+SakthiVijayS@users.noreply.github.com>
2023-10-11 11:35:43 -0400

    Basic

---
+++ application4/src/model/Patient.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shakt
+ */
+public class Patient {
+    String fname;
+    String lname;
+    String age;
+    String type;
+    String gender;
+    String temail;
+    
+    
+}
